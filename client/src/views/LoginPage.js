ff67618f95af85d095468af17dbfa33080d3af51
calumjs1 <71260009+calumjs1@users.noreply.github.com>
2022-01-28 17:45:03 +0000

    link to registration page

--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../store/actions/auth";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -38,21 +39,23 @@ const LoginPage = () => {
             <form onSubmit={onSubmit}>
             <div className="email">
             <i class="fas fa-user"></i>
-              <input type="text" placeholder="user name" className="name" value={username} onChange={(e) => setUsername(e.target.value)}/>
+              <input type="text" placeholder="Username" className="name" value={username} onChange={(e) => setUsername(e.target.value)}/>
             </div>
 
             <div className="password">
             <i class="fas fa-lock"></i>
-              <input type="password" placeholder="password" className="name" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <input type="password" placeholder="Password" className="name" value={password} onChange={(e) => setPassword(e.target.value)}/>
             </div>
 
             <div className="loginRegister-button">
             <button type="submit">Login</button>
           </div></form>
           
+          <ul></ul>
             <p className="link">
-              <a href="#">Forgot password ?</a> Or <a href="#">Sign up</a>
+              <a href="#">Forgot password ?</a> Or <li><Link to="/registration">Sign up</Link></li>
             </p>
+            <ul></ul>
           
         </div>
         </div>
