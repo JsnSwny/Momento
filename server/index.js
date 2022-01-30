b95de105363d41192f66bd94a8673d7e3a9d6ffc
cc164 <cc164@hw.ac.uk>
2022-01-30 09:09:13 +0000

    Canvas API functionality
    I am pretty sure most of this stuff works

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ db.sequelize.sync();
 
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
+require("./routes/project.routes")(app);
+require("./routes/page.routes")(app);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
--- server/models/index.js
+++ server/models/index.js
@@ -8,6 +8,11 @@ const sequelize = new Sequelize(
         host: config.HOST,
         dialect: config.dialect,
 
+        //This has to be here for some reason
+        define: {
+            timestamps: false
+        },
+
         pool: {
           max: config.pool.max,
           min: config.pool.min,
@@ -24,6 +29,8 @@ db.sequelize = sequelize;
 
 db.user = require("./user.model")(sequelize, Sequelize);
 db.role = require("./role.model")(sequelize, Sequelize);
+db.project = require("./project.model")(sequelize, Sequelize);
+db.page = require("./page.model")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
@@ -35,6 +42,14 @@ db.user.belongsToMany(db.role, {
   foreignKey: "userId",
   otherKey: "roleId"
 });
+
+//Connect the user table to the projects table (one to many). The user in this instance is the owner of the project
+db.user.hasMany(db.project, { foreignKey: "projectId" });
+db.project.belongsTo(db.user, { foreignKey: "projectId" });
+
+//Connect the project table to the pages table (one to many)
+db.project.hasMany(db.page, { foreignKey: "pageId" });
+db.page.belongsTo(db.project, { foreignKey: "pageId" })
 
 db.ROLES = ["user", "mod", "admin"]
 
