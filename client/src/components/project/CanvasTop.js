be8bcefe568c5c38a533ac81639dc92e71c03dbe
JsnSwny <jsnswny@gmail.com>
2022-02-03 12:10:51 +0000

    Added clear elements function

--- client/src/components/project/CanvasTop.js
+++ client/src/components/project/CanvasTop.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CanvasActions from "./canvas/CanvasActions";
 import CanvasCurrentlyViewing from "./canvas/CanvasCurrentlyViewing";
 import CanvasPublish from "./canvas/CanvasPublish";
+import { clearElements } from "../../store/reducers/canvas";
 
 const CanvasTop = ({ selectedAction, setSelectedAction, stageRef }) => {
+  const dispatch = useDispatch();
+  const elements = useSelector((state) => state.canvas.elements);
   return (
     <div className="canvas-top flex-container--between">
       <CanvasActions
@@ -12,6 +16,13 @@ const CanvasTop = ({ selectedAction, setSelectedAction, stageRef }) => {
       />
       <div className="flex-container--align-center canvas-top__right">
         <CanvasCurrentlyViewing />
+        {elements.length > 0 && (
+          <i
+            class="fas fa-minus-square"
+            onClick={() => dispatch(clearElements())}
+          ></i>
+        )}
+
         <i
           class="fas fa-save"
           onClick={() => {
