b2e7819a53a642f85ea1cbfc2c0b72ead1d0e6ba
Adrian Szarapow <63786007+Adee1499@users.noreply.github.com>
2022-03-12 16:58:54 +0000

    Calculate comment date difference

--- client/src/components/feed/Comment.js
+++ client/src/components/feed/Comment.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { differenceInMinutes } from "date-fns";
+
+const convertDateDiff = (dateDiff) => {
+  var seconds = Math.floor(dateDiff / 1000);
+  var minutes = Math.floor(seconds / 60);
+  var hours = Math.floor(minutes / 60);
+  var days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return days + " days ago";
+  } else if (hours > 0) {
+    return hours + " hours ago";
+  } else if (minutes > 0) {
+    return minutes + " minutes ago";
+  } else {
+    return seconds + " seconds ago";
+  }
+}
 
 const Comment = ({ obj }) => {
+  var dateDiff = Date.now() - (new Date(obj.commented_at.toString()).getTime());
   return (
     <li className="flex-container">
       <img className="profile-picture" src={obj.imageURL} />
       <div className="feed-post__comment-content">
         <h4>{obj.name}</h4>
         <p>{obj.comment}</p>
-        <small>32m ago</small>
+        <small>{convertDateDiff(dateDiff)}</small>
       </div>
     </li>
   );
