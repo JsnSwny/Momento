3fc572cee3892eb9abf54a89e60ad6de8f202a7c
Adrian Szarapow <63786007+Adee1499@users.noreply.github.com>
2022-03-28 17:47:46 +0100

    Follow / unfollow fix
    Not really the best way to deal with this but there's no time

--- client/src/store/services/user.service.js
+++ client/src/store/services/user.service.js
@@ -14,7 +14,16 @@ function loadUserData(userId, username) {
 };
 
 function followUser(id) {
+    // modify local storage `following` array to reflect changes
+    var userData = JSON.parse(localStorage.getItem('user'));
+    var following = userData.following;
+    var newArray = following.includes(id)
+        ? following.filter((item) => item !== id)
+        : [...following, id];
+    userData.following = newArray;
+    localStorage.setItem('user', JSON.stringify(userData));
+
     return api.post(`/followUser`, {
         id
-    });
+    })
 };
